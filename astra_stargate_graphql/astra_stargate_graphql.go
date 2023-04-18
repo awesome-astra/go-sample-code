32da1053435da95ff18739b79b568485c00f2e55
synedra-datastax <synedra@gmail.com>
2023-04-18 10:03:28 -0700

    Using new library path

--- astra_stargate_graphql/astra_stargate_graphql.go
+++ astra_stargate_graphql/astra_stargate_graphql.go
@@ -2,23 +2,21 @@ package main
 
 import (
 	"bytes"
-	// "encoding/json"
 	"fmt"
 	"os"
 
-	"github.com/synedra/astra_stargate"
-
+	"github.com/awesome-astra/astra_stargate_go"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if len(os.Getenv("ASTRA_DB_APPLICATION_TOKEN")) == 0 {
-		fmt.Println(fmt.Errorf("please set your environment variables or use astra db create-dotenv to create a .env file"))
+		fmt.Println(fmt.Errorf("please set your environment variables or use 'astra db create-dotenv' to create a .env file"))
 		return
 	}
 
-	client := astra_stargate.NewBasicAuthClient(os.Getenv("ASTRA_DB_APPLICATION_TOKEN"), os.Getenv("ASTRA_DB_ID"), os.Getenv("ASTRA_DB_REGION"))
+	client := astra_stargate_go.NewBasicAuthClient(os.Getenv("ASTRA_DB_APPLICATION_TOKEN"), os.Getenv("ASTRA_DB_ID"), os.Getenv("ASTRA_DB_REGION"))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -31,7 +29,9 @@ func main() {
 		panic(err)
 	}
 	req, err := client.APIPost("/api/graphql-schema", bodyReader)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(req)
-	fmt.Println(err)
 
 }
--- astra_stargate_graphql/go.mod
+++ astra_stargate_graphql/go.mod
@@ -4,5 +4,5 @@ go 1.20
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/synedra/astra_stargate v0.0.0-20230413203949-fe482074c683
+	github.com/awesome-astra/astra_stargate_go v0.0.3
 )
