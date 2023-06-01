5e0fdeeeea267a64a4a41b117b76a95c088aaa03
synedra-datastax <synedra@gmail.com>
2023-06-01 10:34:03 -0700

    Example for gocql-astra

--- gocql-astra/envvars/envvars.go
+++ gocql-astra/envvars/envvars.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -16,46 +15,54 @@ func main() {
 	var err error
 
 	err = godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
 	var cluster *gocql.ClusterConfig
-	if len(os.Getenv("ASTRA_DB_SECURE_BUNDLE_PATH")) > 0 {
-		cluster, err = gocqlastra.NewClusterFromBundle(os.Getenv("ASTRA_DB_SECURE_BUNDLE_PATH"), "token", os.Getenv("ASTRA_DB_APPLICATION_TOKEN"), 10*time.Second)
-		if err != nil {
-			err = fmt.Errorf("unable to open bundle %s from file: %v", os.Getenv("ASTRA_DB_SECURE_BUNDLE_PATH"), err)
-			panic(err)
-		}
-	} else if len(os.Getenv("ASTRA_DB_APPLICATION_TOKEN")) > 0 {
+
+	if len(os.Getenv("ASTRA_DB_APPLICATION_TOKEN")) > 0 {
+
 		if len(os.Getenv("ASTRA_DB_ID")) == 0 {
 			panic("database ID is required when using a token")
 		}
-		cluster, err = gocqlastra.NewClusterFromURL("https://api.astra.datastax.com", os.Getenv("ASTRA_DB_ID"), os.Getenv("ASTRA_DB_APPLICATION_TOKEN"), 10*time.Second)
-		fmt.Println(cluster)
+	}
+
+	fmt.Println("Creating the cluster now")
+	fmt.Println(os.Getenv("ASTRA_DB_ID"))
+	fmt.Print(os.Getenv("ASTRA_DB_APPLICATION_TOKEN"))
+	fmt.Println(os.Getenv("ASTRA_DB_REGION"))
+	fmt.Println(cluster)
+
+	cluster, err = gocqlastra.NewClusterFromURL("https://api.astra.datastax.com", os.Getenv("ASTRA_DB_ID"), os.Getenv("ASTRA_DB_APPLICATION_TOKEN"), 10*time.Second)
+	fmt.Println(cluster)
+	/*
 		if err != nil {
 			fmt.Errorf("unable to load cluster %s from astra: %v", os.Getenv("ASTRA_DB_APPLICATION_TOKEN"), err)
 		}
-	} else {
-		fmt.Errorf("must provide either bundle path or token")
-	}
 
-	start := time.Now()
-	session, err := gocql.NewSession(*cluster)
-	elapsed := time.Now().Sub(start)
-	if err != nil {
-		log.Fatalf("unable to connect session: %v", err)
-	}
+		start := time.Now()
+		session, err := gocql.NewSession(*cluster)
+		elapsed := time.Now().Sub(start)
 
-	fmt.Println("Making the query now")
+		if err != nil {
+			log.Fatalf("unable to connect session: %v", err)
+		}
 
-	iter := session.Query("SELECT release_version FROM system.local").Iter()
+		fmt.Println("Making the query now")
 
-	var version string
-	for iter.Scan(&version) {
-		fmt.Println(version)
-	}
+		iter := session.Query("SELECT release_version FROM system.local").Iter()
 
-	if err = iter.Close(); err != nil {
-		log.Printf("error running query: %v", err)
-	}
+		var version string
 
-	fmt.Printf("Connection process took %s\n", elapsed)
+		for iter.Scan(&version) {
+			fmt.Println(version)
+		}
+
+		if err = iter.Close(); err != nil {
+			log.Printf("error running query: %v", err)
+		}
+
+		fmt.Printf("Connection process took %s\n", elapsed)
+	*/
 }
